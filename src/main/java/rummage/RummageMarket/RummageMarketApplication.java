5425d35ce21227d1dcf6740624bce6d8eae245e0
yewon <ajdanddl6321@gmail.com>
2022-11-21 04:17:15 +0900

    초기세팅완료

---
+++ src/main/java/rummage/RummageMarket/RummageMarketApplication.java
@@ -0,0 +1,13 @@
+package rummage.RummageMarket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RummageMarketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RummageMarketApplication.class, args);
+	}
+
+}
---
+++ src/test/java/rummage/RummageMarket/RummageMarketApplicationTests.java
@@ -0,0 +1,13 @@
+package rummage.RummageMarket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RummageMarketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
