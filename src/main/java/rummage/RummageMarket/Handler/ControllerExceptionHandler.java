b09c9c63de9770559d61307b900021cf2300fa39
yewon <ajdanddl6321@gmail.com>
2022-11-22 02:07:52 +0900

    feat: CMRespDto(공통응답Dto) 생성

--- src/main/java/rummage/RummageMarket/Handler/ControllerExceptionHandler.java
+++ src/main/java/rummage/RummageMarket/Handler/ControllerExceptionHandler.java
@@ -7,14 +7,15 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestController;
 
 import rummage.RummageMarket.Handler.Ex.CustomValidationException;
+import rummage.RummageMarket.Web.Dto.CMRespDto;
 
 @RestController
 @ControllerAdvice // 모든 exception을 다 낚아채는 어노테이션
 public class ControllerExceptionHandler {
 
 	@ExceptionHandler(CustomValidationException.class)
-	public Map<String, String> validationException(CustomValidationException e) {
-		return e.getErrorMap();
+	public CMRespDto<?> validationException(CustomValidationException e) {
+		return new CMRespDto(-1, e.getMessage(), e.getErrorMap());
 	}
-	
+
 }
