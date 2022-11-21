cff063ead38f51fbd3c0e0faa5a5304ad298d28e
yewon <ajdanddl6321@gmail.com>
2022-11-22 03:18:27 +0900

    chore: PrincipalDetailsService에 UserRepository DI

--- src/main/java/rummage/RummageMarket/Config/Auth/PrincipalDetailsService.java
+++ src/main/java/rummage/RummageMarket/Config/Auth/PrincipalDetailsService.java
@@ -5,8 +5,14 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
+import lombok.RequiredArgsConstructor;
+import rummage.RummageMarket.Domain.User.UserRepository;
+
+@RequiredArgsConstructor
 @Service
 public class PrincipalDetailsService implements UserDetailsService{
+	
+	private final UserRepository userRepository;
 
 	@Override
 	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
