3e8e91aefc0f8fb2b2ad11e3e78870814cd7c06d
yewon <ajdanddl6321@gmail.com>
2022-11-21 23:26:56 +0900

    feat: PostMapping signup() 추가

--- src/main/java/rummage/RummageMarket/Web/AuthController.java
+++ src/main/java/rummage/RummageMarket/Web/AuthController.java
@@ -1,6 +1,8 @@
 package rummage.RummageMarket.Web;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class AuthController {
@@ -13,5 +15,10 @@ public class AuthController {
 	@GetMapping("/auth/signup")
 	public String signupForm() {
 		return "auth/signup";
+	}
+	
+	@PostMapping("/auth/signup")
+	public String signup() {
+		return "auth/signin";
 	}
 }
