d83effeb915128a56158af801898f33868bd074d
yewon <ajdanddl6321@gmail.com>
2022-11-22 02:12:53 +0900

    chore: CMRespDto에서 Script로 변경

--- src/main/java/rummage/RummageMarket/Handler/ControllerExceptionHandler.java
+++ src/main/java/rummage/RummageMarket/Handler/ControllerExceptionHandler.java
@@ -7,15 +7,15 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestController;
 
 import rummage.RummageMarket.Handler.Ex.CustomValidationException;
-import rummage.RummageMarket.Web.Dto.CMRespDto;
+import rummage.RummageMarket.Util.Script;
 
 @RestController
 @ControllerAdvice // 모든 exception을 다 낚아채는 어노테이션
 public class ControllerExceptionHandler {
 
 	@ExceptionHandler(CustomValidationException.class)
-	public CMRespDto<?> validationException(CustomValidationException e) {
-		return new CMRespDto(-1, e.getMessage(), e.getErrorMap());
+	public String validationException(CustomValidationException e) {
+		return Script.back(e.getErrorMap().toString());
 	}
 
 }
