cf0d64a8f6eb6400993a181df946f36d0dfeef5d
yewon <ajdanddl6321@gmail.com>
2022-11-22 00:00:56 +0900

    feat: SignupDto toEntity() 생성, 데이터 들어왔는지 확인

--- src/main/java/rummage/RummageMarket/Web/AuthController.java
+++ src/main/java/rummage/RummageMarket/Web/AuthController.java
@@ -1,11 +1,19 @@
 package rummage.RummageMarket.Web;
 
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import rummage.RummageMarket.Domain.User.User;
+import rummage.RummageMarket.Web.Dto.Auth.SignupDto;
+
 @Controller
 public class AuthController {
+	
+	private static final Logger log = LoggerFactory.getLogger(AuthController.class);
 
 	@GetMapping("/auth/signin")
 	public String signinForm() {
@@ -18,7 +26,10 @@ public class AuthController {
 	}
 	
 	@PostMapping("/auth/signup")
-	public String signup() {
+	public String signup(SignupDto signupDto) {
+		log.info(signupDto.toString());
+		User user = signupDto.toEntity();
+		log.info(user.toString());
 		return "auth/signin";
 	}
 }
--- src/main/java/rummage/RummageMarket/Web/Dto/Auth/SignupDto.java
+++ src/main/java/rummage/RummageMarket/Web/Dto/Auth/SignupDto.java
@@ -1,10 +1,19 @@
 package rummage.RummageMarket.Web.Dto.Auth;
 
 import lombok.Data;
+import rummage.RummageMarket.Domain.User.User;
 
 @Data
 public class SignupDto {
 	private String username;
 	private String password;
 	private String nickname;
+	
+	public User toEntity() {
+		return User.builder()
+				.username(username)
+				.password(password)
+				.nickname(nickname)
+				.build();
+	}
 }
