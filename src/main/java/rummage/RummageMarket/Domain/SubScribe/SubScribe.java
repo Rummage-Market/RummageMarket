6ab149340eed420a1a7cf2e22b2b0a6c3f0a6740
Glory <dudrhkd6550@naver.com>
2022-11-23 21:03:55 +0900

    feat: SubScribeRepository 생성

--- src/main/java/rummage/RummageMarket/Domain/SubScribe/SubScribe.java
+++ src/main/java/rummage/RummageMarket/Domain/SubScribe/SubScribe.java
@@ -11,7 +11,6 @@ import javax.persistence.ManyToOne;
 import javax.persistence.PrePersist;
 import javax.persistence.Table;
 import javax.persistence.UniqueConstraint;
-
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
