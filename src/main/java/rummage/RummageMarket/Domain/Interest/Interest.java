c63f55fb620870eaae9116f16c516eb409f95c68
yewon <ajdanddl6321@gmail.com>
2022-11-26 22:23:33 +0900

    feat: Interset 모델 생성

---
+++ src/main/java/rummage/RummageMarket/Domain/Interest/Interest.java
@@ -0,0 +1,46 @@
+package rummage.RummageMarket.Domain.Interest;
+
+import java.time.LocalDateTime;
+
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.PrePersist;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import rummage.RummageMarket.Domain.Post.Post;
+import rummage.RummageMarket.Domain.User.User;
+
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Entity
+public class Interest {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    
+    @JoinColumn(name = "userId")
+    @ManyToOne(fetch = FetchType.EAGER)
+    private User user;
+    
+    @JoinColumn(name = "postId")
+    @ManyToOne(fetch = FetchType.EAGER)
+    private Post post;
+    
+    private LocalDateTime createDate;
+
+    @PrePersist
+    public void createDate() {
+        this.createDate = LocalDateTime.now();
+    }
+}
