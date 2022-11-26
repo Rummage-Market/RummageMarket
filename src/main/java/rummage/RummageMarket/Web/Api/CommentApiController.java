89861223cc9ff3435b10110d15da694cae230b00
Glory <dudrhkd6550@naver.com>
2022-11-27 01:24:54 +0900

    feat: Comment 작성 유효성 검사

--- src/main/java/rummage/RummageMarket/Web/Api/CommentApiController.java
+++ src/main/java/rummage/RummageMarket/Web/Api/CommentApiController.java
@@ -1,9 +1,16 @@
 package rummage.RummageMarket.Web.Api;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -13,6 +20,7 @@ import org.springframework.web.bind.annotation.RestController;
 import lombok.RequiredArgsConstructor;
 import rummage.RummageMarket.Config.Auth.PrincipalDetails;
 import rummage.RummageMarket.Domain.Comment.Comment;
+import rummage.RummageMarket.Handler.Ex.CustomValidationApiException;
 import rummage.RummageMarket.Service.CommentService;
 import rummage.RummageMarket.Web.Dto.CMRespDto;
 import rummage.RummageMarket.Web.Dto.Comment.CommentDto;
@@ -20,17 +28,27 @@ import rummage.RummageMarket.Web.Dto.Comment.CommentDto;
 @RequiredArgsConstructor
 @RestController
 public class CommentApiController {
-    
+
     @Autowired
-    CommentService commentService; 
-    
+    CommentService commentService;
+
     @PostMapping("/api/comment")
-    public ResponseEntity<?> commentSave(@RequestBody CommentDto commentDto,@AuthenticationPrincipal PrincipalDetails principalDetails){
-        
-        Comment comment = commentService.commentSave(commentDto.getContent(),commentDto.getPostId(),principalDetails.getUser().getId());
-        return new ResponseEntity<>(new CMRespDto<>(1, "댓글쓰기 성공",comment), HttpStatus.CREATED);
+    public ResponseEntity<?> commentSave(@Valid @RequestBody CommentDto commentDto, BindingResult bindingResult,
+            @AuthenticationPrincipal PrincipalDetails principalDetails) {
+
+        if (bindingResult.hasErrors()) {
+            Map<String, String> errorMap = new HashMap<>();
+            for (FieldError error : bindingResult.getFieldErrors()) {
+                errorMap.put(error.getField(), error.getDefaultMessage());
+            }
+            throw new CustomValidationApiException("유효성검사 실패", errorMap);
+        }
+
+        Comment comment = commentService.commentSave(commentDto.getContent(), commentDto.getPostId(),
+                principalDetails.getUser().getId());
+        return new ResponseEntity<>(new CMRespDto<>(1, "댓글쓰기 성공", comment), HttpStatus.CREATED);
     }
-    
+
     @DeleteMapping("/api/comment/{id}")
     public ResponseEntity<?> commentDelete(@PathVariable int id) {
         commentService.commentDelete(id);
