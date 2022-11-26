2f98d07927c005bd8ca408461bd19691755fa9a3
yewon <ajdanddl6321@gmail.com>
2022-11-27 01:29:55 +0900

    feat: InterestService에서 haveInterest(), haveNoInterest() 생성,  InterestRepostiory 네이티브쿼리 사용

--- src/main/java/rummage/RummageMarket/Domain/Interest/InterestRepostiory.java
+++ src/main/java/rummage/RummageMarket/Domain/Interest/InterestRepostiory.java
@@ -1,7 +1,16 @@
 package rummage.RummageMarket.Domain.Interest;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 
 public interface InterestRepostiory extends JpaRepository<Interest, Integer>{
-
+    
+    @Modifying
+    @Query(value = "INSERT INTO likes(postId, userId) VALUES(:postId, :principalId)", nativeQuery = true)
+    int mInterest(int imageId, int principalId);
+    
+    @Modifying
+    @Query(value = "DELETE FROM likes WHERE postId = :postId AND userId = :principalId", nativeQuery = true)
+    int mDisinterest(int imageId, int principalId);
 }
