1009df3e283d938c3dd377a6338fd0bcbfcc13a5
yewon <ajdanddl6321@gmail.com>
2022-11-27 01:21:53 +0900

    feat: PostApiController에서 interest, disinterest 생성

--- src/main/java/rummage/RummageMarket/Web/Api/PostApiController.java
+++ src/main/java/rummage/RummageMarket/Web/Api/PostApiController.java
@@ -7,25 +7,45 @@ import org.springframework.data.domain.Sort;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import lombok.RequiredArgsConstructor;
+import rummage.RummageMarket.Config.Auth.PrincipalDetails;
 import rummage.RummageMarket.Domain.Post.Post;
+import rummage.RummageMarket.Service.InterestService;
 import rummage.RummageMarket.Service.PostService;
 import rummage.RummageMarket.Web.Dto.CMRespDto;
 
 @RestController
-@RequiredArgsConstructor
 public class PostApiController {
     
     @Autowired
     PostService postService;
+    
+    @Autowired
+    InterestService interestService;
     
     //필터링 없이 제일 최신순의 게시글
     @GetMapping("/api/post")
     public ResponseEntity<?> postStory(@PageableDefault(size = 3) Pageable pageable) {
         Page<Post> posts= postService.postList(pageable);
         return new ResponseEntity<>(new CMRespDto<>(1, "게시글 불러오기 성공", posts), HttpStatus.OK);
+    }
+    
+    @PostMapping("/api/post/{postId}/interest")
+    public ResponseEntity<?> interest(@AuthenticationPrincipal PrincipalDetails principalDetails, @PathVariable int postId) {
+        interestService.haveInterest(postId, principalDetails.getUser().getId());
+        return new ResponseEntity<>(new CMRespDto<>(1, "interest 성공", null), HttpStatus.OK);
+    }
+    
+    @DeleteMapping("/api/post/{postId}/interest")
+    public ResponseEntity<?> disinterest(@AuthenticationPrincipal PrincipalDetails principalDetails, @PathVariable int postId) {
+        interestService.haveNoInterest(postId,  principalDetails.getUser().getId());
+        return new ResponseEntity<>(new CMRespDto<>(1, "disinterest 성공", null), HttpStatus.OK);
     }
 }
