32c03426aac5b880d92c5834936182eb4214f29e
Glory <dudrhkd6550@naver.com>
2022-11-28 18:58:42 +0900

    feat: 유저 프로필사진 변경 Controller&Service

--- src/main/java/rummage/RummageMarket/Service/UserService.java
+++ src/main/java/rummage/RummageMarket/Service/UserService.java
@@ -1,13 +1,21 @@
 package rummage.RummageMarket.Service;
 
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.UUID;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.multipart.MultipartFile;
 
 import rummage.RummageMarket.Domain.Neighbor.NeighborRepository;
 import rummage.RummageMarket.Domain.User.User;
 import rummage.RummageMarket.Domain.User.UserRepository;
+import rummage.RummageMarket.Handler.Ex.CustomApiException;
 import rummage.RummageMarket.Handler.Ex.CustomException;
 import rummage.RummageMarket.Handler.Ex.CustomValidationApiException;
 import rummage.RummageMarket.Web.Dto.User.UserProfileDto;
@@ -63,4 +71,31 @@ public class UserService {
 	    userEntity.setEmail(user.getEmail());
 	    return userEntity;
 	}
+
+	@Value("${file.path}")
+    private String uploadFolder;
+	
+    @Transactional
+    public User profileImageUrlUpdate(int principalId, MultipartFile profileImageFile) {
+        UUID uuid = UUID.randomUUID(); // uuid
+        String imageFileName = uuid + "_" + profileImageFile.getOriginalFilename(); // 1.jpg
+        System.out.println("이미지 파일이름 : " + imageFileName);
+
+        Path imageFilePath = Paths.get(uploadFolder + imageFileName);
+
+        // 통신, I/O -> 예외가 발생할 수 있다.
+        try {
+            Files.write(imageFilePath, profileImageFile.getBytes());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        User userEntity = userRepository.findById(principalId).orElseThrow(() -> {
+            // throw -> return 으로 변경
+            return new CustomApiException("유저를 찾을 수 없습니다.");
+        });
+        userEntity.setProfileImage(imageFileName);
+
+        return userEntity;
+    } // 더티체킹으로 업데이트 됨.
 }
